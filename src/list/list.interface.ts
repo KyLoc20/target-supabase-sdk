c173000c2e5c501b8d1f7a779fc391e41dc9a7ff
liuzeduo <liuzeduo1234@gmail.com>
2026-06-14 23:09:22 +0900

    fix: ListDetails

--- src/list/list.interface.ts
+++ src/list/list.interface.ts
@@ -19,6 +19,6 @@ export interface ListDetails {
     preview: string;
     /** How to render the List */
     loaderKey: string;
-    items: Array<Omit<Target, 'id' | 'created_at'>>[]
+    items: Array<Omit<Target, 'id' | 'created_at'>>
 };
 
